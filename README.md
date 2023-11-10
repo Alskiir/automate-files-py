f54851eef6aaeb111b56a3286345fd6b72544233
Alskiir <doughart215@cfl.rr.com>
2023-11-10 13:33:44 -0500

    Added functionality

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Automated File Handler
 
-This Python script automatically moves `.unitypackage` files from a specified source directory to a destination directory. It uses the `watchdog` library to monitor the source directory for any new files.
+This Python script automatically moves files from a specified source directory to different destination directories based on their file extensions. It uses the `watchdog` library to monitor the source directory for any new files.
 
 ## Dependencies
 
@@ -15,13 +15,15 @@ pip install watchdog
 
 ## Usage
 
-By default, the script watches the `C:/Users/dough/Downloads` directory and moves any new `.unitypackage` files to `C:/Users/dough/Documents/Unity Packages`.
+By default, the script watches the `C:/Users/dough/Downloads` directory and moves any new `.unitypackage` files to `C:/Users/dough/Documents/Unity Packages` and `.txt` files to `C:/Users/dough/Documents/Text Files`.
 
 You can change these directories by modifying the main function in the script:
 ```bash
-def main(folder_to_watch="your/source/directory"):
-    event_handler = FileHandler("your/destination/directory")
-    ...
+destinations = {
+    ".unitypackage": "your/destination/directory/for/unitypackage/files",
+    ".txt": "your/destination/directory/for/txt/files",
+    # Add more file extensions and destinations as needed
+}
 ```
 
 To run the script, use the following command:
@@ -30,4 +32,7 @@ To run the script, use the following command:
 python automate-files.py
 ```
 
-The script will run indefinitely until you stop it with `Ctrl+C`
+## Additional Notes
+
+- The script will run indefinitely until you stop it with `Ctrl+C`.
+- If a destination directory does not exist, the script will create it.
--- automate-files.py
+++ automate-files.py
@@ -1,31 +1,44 @@
 import time
 import shutil
+import os
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 class FileHandler(FileSystemEventHandler):
     """
-    A class that handles file system events, such as file creation, and moves .unitypackage files to a specified folder.
+    A class that handles file system events, such as file creation, 
+    and moves files to specified folders based on their extensions.
     """
-    def __init__(self, destination_folder):
-        self.destination_folder = destination_folder
+    
+    def __init__(self, destinations):
+        self.destinations = destinations
 
     def on_created(self, event):
-        if not event.is_directory:  # Check if the event is not a directory (i.e., a file)
+        # Check if the event is not a directory (i.e., a file)
+        if not event.is_directory:
             file_path = event.src_path
-            if file_path.endswith(".unitypackage"):  # Search for .unitypackage files
-                self.move_file(file_path, self.destination_folder)
+            file_extension = os.path.splitext(file_path)[1]
+            # Check if the file extension is in the destinations dictionary
+            if file_extension in self.destinations:  
+                self.move_file(file_path, self.destinations[file_extension])
 
-    # Move the file from the source to the destination folder
-    def move_file(self, source):
+    def move_file(self, source, destination):
         try:
-            shutil.move(source, self.destination_folder)
-            print(f"Moved file from {source} to {self.destination_folder}")
+            # Create the destination folder if it doesn't exist
+            os.makedirs(destination, exist_ok=True)  
+            # Move the file from the source to the destination folder
+            shutil.move(source, destination)
+            print(f"Moved file from {source} to {destination}")
         except Exception as e:
             print(f"Error moving file: {e}")
 
 def main(folder_to_watch="C:/Users/dough/Downloads"):
-    event_handler = FileHandler("C:/Users/dough/Documents/Unity Packages")
+    destinations = {
+        ".unitypackage": "C:/Users/dough/Documents/Unity Packages",
+        ".txt": "C:/Users/dough/Documents/Text Files",
+        # Add more file extensions and destinations as needed
+    }
+    event_handler = FileHandler(destinations)
     observer = Observer()
     observer.schedule(event_handler, folder_to_watch, recursive=False)
     observer.start()
